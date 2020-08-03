b9ce498bf0bc22c76b377cc3697ea0d0571d5c84
francis_xiiiv <francis_xiiiv@163.com>
2020-08-02 23:55:35 -0700

    util to sign text message

--- Exodus-fullnode/localfullnode2/src/main/java/org/exodus/localfullnode2/utilities/Signer.java
+++ Exodus-fullnode/localfullnode2/src/main/java/org/exodus/localfullnode2/utilities/Signer.java
@@ -4,9 +4,6 @@ import java.math.BigInteger;
 import java.time.Instant;
 
 import org.bitcoinj.core.ECKey;
-
-import com.alibaba.fastjson.JSONObject;
-
 import org.exodus.bean.message.MessageType;
 import org.exodus.bean.message.MessageVersion;
 import org.exodus.bean.message.TransactionMessage;
@@ -14,9 +11,11 @@ import org.exodus.core.Constant;
 import org.exodus.utils.DSA;
 import org.exodus.utils.SignUtil;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * 
- *  All rights reserved.
+ * All rights reserved.
  * 
  * @Description: signature tools which are able to sign all kinds of messages
  *               using existed public-private key pair.
@@ -39,7 +38,7 @@ public class Signer {
 	 * signing a "2.0" transaction message
 	 */
 	public String signTx(String fromAddress, String toAddress, BigInteger amount, BigInteger fee, BigInteger nrgPrice) {
-		JSONObject json = getMessage("2.0", fromAddress, toAddress, amount, fee, nrgPrice);
+		JSONObject json = buildMessage("2.0", fromAddress, toAddress, amount, fee, nrgPrice);
 
 		String msgSignature = SignUtil.sign(json.toJSONString(), ECKey.fromPrivate(DSA.decryptBASE64(sPrivateKey)));
 
@@ -48,7 +47,44 @@ public class Signer {
 		return json.toJSONString();
 	}
 
-	private JSONObject getMessage(String ver, String fromAddress, String toAddress, BigInteger amount, BigInteger fee,
+	/**
+	 * signing a "2.0" text message
+	 */
+	public String signTextTx(String fromAddress, byte[] context) {
+		JSONObject json = buildTextMessage("2.0", fromAddress, context);
+
+		String msgSignature = SignUtil.sign(json.toJSONString(), ECKey.fromPrivate(DSA.decryptBASE64(sPrivateKey)));
+
+		json.put("signature", msgSignature);
+
+		return json.toJSONString();
+	}
+
+	private JSONObject buildTextMessage(String ver, String fromAddress, byte[] context) {
+		JSONObject json = new JSONObject();
+		json.put("fromAddress", fromAddress);
+		// json.put("toAddress", toAddress);
+		json.put("amount", "0");
+		json.put("timestamp", Instant.now().toEpochMilli());
+		json.put("pubkey", sPublicKey);
+		if (MessageVersion.DEV_1_0.equals(ver)) {
+			json.put("fee", "0");
+		} else if (MessageVersion.PRO_2_0.equals(ver)) {
+			if (Constant.NEED_FEE) {
+				json.put("fee", "0");
+				json.put("nrgPrice", "0");
+			}
+		}
+
+		json.put("type", MessageType.TEXT.getIndex());
+		json.put("vers", ver);
+
+		json.put("context", context);
+
+		return json;
+	}
+
+	private JSONObject buildMessage(String ver, String fromAddress, String toAddress, BigInteger amount, BigInteger fee,
 			BigInteger nrgPrice) {
 		JSONObject json = new JSONObject();
 		json.put("fromAddress", fromAddress);
