c6f38c25730fd1bede344f542becef934e18dcb0
francis_xiiiv <francis_xiiiv@163.com>
2020-08-04 20:27:34 -0700

    terminate akka system by reporter

--- Exodus-fullnode/plotter4pos/src/main/java/org/exodus/plotter4pos/miner/PlotReporter.java
+++ Exodus-fullnode/plotter4pos/src/main/java/org/exodus/plotter4pos/miner/PlotReporter.java
@@ -3,6 +3,8 @@ package org.exodus.plotter4pos.miner;
 import org.exodus.plotter4pos.miner.GenSupervisor.msgPlantResult;
 
 import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 /**
  * 
@@ -15,11 +17,15 @@ import akka.actor.AbstractActor;
  *
  */
 public class PlotReporter extends AbstractActor {
+	private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
 	@Override
 	public Receive createReceive() {
 		return this.receiveBuilder().match(msgPlantResult.class, m -> {
 			// start to report disk occupation
+
+			log.info("starting terminating...");
+			getContext().system().terminate();
 		}).build();
 	}
 
